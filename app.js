8aade72b34f57fbf5c2de9f93f3c11f74211ac18
kintrp <philipp.kintrup@googlemail.com>
2021-07-28 17:26:35 +0200

    build

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ const app = express();
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 // session configuration
 
 const session = require('express-session');
@@ -101,5 +104,10 @@ app.use("/api/auth", authRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
+
+app.use((req, res) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/client/build/index.html");
+  });
 
 module.exports = app;
